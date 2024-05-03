fdb65ef54e7263ee90a9d97256b0b31c937b0f79
mvtvn78 <mvtvn78@gmail.com>
2024-05-03 21:34:50 +0700

    Liricle Lib

--- JSlyricsforAudio/index.js
+++ JSlyricsforAudio/index.js
@@ -1,4 +1,37 @@
-new RabbitLyrics({
-    element: document.findElementById('lyrics-1'),
-    mediaElement: document.findElementById('audio-1')
-})
+// get DOM elements
+const $audio = document.querySelector(".audio");
+const $lyric_word = document.querySelector(".lyric-word");
+const $lyric_line = document.querySelector(".lyric-line");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// create Liricle instance
+const liricle = new Liricle();
+
+// listen to on load event
+liricle.on("load", data => {
+      console.log(data);
+});
+
+// listen to on sync event
+liricle.on("sync", (line, word) => {
+      console.log("current line => ", line);
+      console.log("current word => ", word);
+      $lyric_line.innerText = line.text;
+      // please always check the word value before using it.
+      if (word) $lyric_word.innerText = word.text;
+});
+
+// load lyric
+liricle.load({ url: "lyrics_enhance.lrc" });
+
+// adjust lyric speed
+// positive value => speed up
+// negative value ​​=> slow down
+liricle.offset = 200; // value in milliseconds.
+
+$audio.addEventListener("timeupdate", () => {
+    const time = $audio.currentTime;
+    // sync lyric when the audio time updated
+    liricle.sync(time, false);
+});
